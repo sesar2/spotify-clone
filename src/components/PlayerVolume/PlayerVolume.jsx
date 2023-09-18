32e9a3176f77f9ec0273a147853568a7ec330d2e
sesar <54498315+sesar2@users.noreply.github.com>
2023-09-18 22:12:40 +0200

    mute

--- src/components/PlayerVolume/PlayerVolume.jsx
+++ src/components/PlayerVolume/PlayerVolume.jsx
@@ -1,4 +1,4 @@
-import { Slider, Stack } from "@mui/material";
+import { Slider, Stack, IconButton } from "@mui/material";
 import {VolumeDown, VolumeUp, VolumeOff} from "@mui/icons-material"
 import { useState } from "react";
 
@@ -14,7 +14,9 @@ const PlayerVolume = ({spotifyApi, player}) => {
     }
     return ( 
         <Stack direction={'row'} spacing={2} alignItems={'center'} sx={{width: 150, color: 'text.secondary', }}>
+            <IconButton onClick={(prev)=>{volume > 0 ? setVolume(0) : setVolume(prev)}}>
             {volume === 0 ? <VolumeOff/> : volume < 50 ? <VolumeDown/> : <VolumeUp/>} 
+            </IconButton>
             <Slider min={0} max={100} step={1} value={volume} onChange={(e, value)=>setVolume(value)} onChangeCommitted={async(e, value)=>{handleVolumeChange(value)}}/>   
         </Stack>
      );
