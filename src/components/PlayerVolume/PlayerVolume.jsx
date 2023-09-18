bb67ba987fece7f8afe875f9f1a8f3261a9f1257
sesar <54498315+sesar2@users.noreply.github.com>
2023-09-18 22:20:18 +0200

    mute previous volume

--- src/components/PlayerVolume/PlayerVolume.jsx
+++ src/components/PlayerVolume/PlayerVolume.jsx
@@ -1,10 +1,20 @@
 import { Slider, Stack, IconButton } from "@mui/material";
 import {VolumeDown, VolumeUp, VolumeOff} from "@mui/icons-material"
-import { useState } from "react";
+import { useState, useRef } from "react";
 
 const PlayerVolume = ({spotifyApi, player}) => {
     const [volume, setVolume] = useState(50)
 
+    function usePrevious(value) {
+        const ref = useRef();
+        useEffect(() => {
+          ref.current = value; //assign the value of ref to the argument
+        },[value]); //this code will run when the value of 'value' changes
+        return ref.current; //in the end, return the current ref value.
+      }
+    const preVol = usePrevious(volume)
+
+    
     const handleVolumeChange = async (value) => {
         try {
             await player.setVolume(value / 100)
@@ -14,7 +24,7 @@ const PlayerVolume = ({spotifyApi, player}) => {
     }
     return ( 
         <Stack direction={'row'} spacing={2} alignItems={'center'} sx={{width: 150, color: 'text.secondary', }}>
-            <IconButton sx={{ color: 'text.primary' }} onClick={(prev)=>{volume > 0 ? setVolume(0) : setVolume(50)}}>
+            <IconButton sx={{ color: 'text.primary' }} onClick={()=>{volume > 0 ? setVolume(0) : setVolume(preVol)}}>
             {volume === 0 ? <VolumeOff/> : volume < 50 ? <VolumeDown/> : <VolumeUp/>} 
             </IconButton>
             <Slider min={0} max={100} step={1} value={volume} onChange={(e, value)=>setVolume(value)} onChangeCommitted={async(e, value)=>{handleVolumeChange(value)}}/>   
