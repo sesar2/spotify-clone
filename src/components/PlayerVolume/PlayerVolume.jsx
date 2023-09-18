4d385d6970f15b74afa0597ada3fb7b2cef3f6f6
sesar <54498315+sesar2@users.noreply.github.com>
2023-09-18 22:23:53 +0200

    fix

--- src/components/PlayerVolume/PlayerVolume.jsx
+++ src/components/PlayerVolume/PlayerVolume.jsx
@@ -24,7 +24,7 @@ const PlayerVolume = ({spotifyApi, player}) => {
     }
     return ( 
         <Stack direction={'row'} spacing={2} alignItems={'center'} sx={{width: 150, color: 'text.secondary', }}>
-            <IconButton sx={{ color: 'text.primary' }} onClick={()=>{volume > 0 ? setVolume(0) : setVolume(preVol)}}>
+            <IconButton sx={{ color: 'text.primary' }} onClick={()=>{volume > 0 ? setVolume(0) : setVolume(()=>volume === 0 ? 25 : preVol)}}>
             {volume === 0 ? <VolumeOff/> : volume < 50 ? <VolumeDown/> : <VolumeUp/>} 
             </IconButton>
             <Slider min={0} max={100} step={1} value={volume} onChange={(e, value)=>setVolume(value)} onChangeCommitted={async(e, value)=>{handleVolumeChange(value)}}/>   
