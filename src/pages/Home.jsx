3f55008a72ba57d9a0800073f5809c301377b95a
sesar <54498315+sesar2@users.noreply.github.com>
2024-02-14 13:09:36 +0100

    fix

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -20,13 +20,8 @@ const Home = () => {
 				gap: 5
 			}}
 		>
-			<Avatar
-				sizes="large"
-				src="/image0 (2).jpg"
-				alt="profilbild"
-				sx={{ maxHeight: '50%', maxWidth: '50%', height: 200, width: 200 }}
-			/>
-			<Button size="large" variant="contained" href="https://lively-shortbread-5a5d4f.netlify.app">
+	
+			<Button size="large" variant="contained" href="https://isacdanielssonportfolio.netlify.app/">
 				My portfolio
 			</Button>
 			{openLogout ? (
